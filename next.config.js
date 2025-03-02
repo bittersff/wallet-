ba3dd367f870e39228d44bad41c8440b84db6b4d
bittersff <ayyubtorn@gmail.com>
2025-03-02 00:10:08 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,7 @@ const nextConfig = {
       ...config.resolve.fallback,
       fs: false,
       net: false,
-      tls: false,
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      zlib: require.resolve('browserify-zlib'),
-      os: require.resolve('os-browserify/browser'),
-      path: require.resolve('path-browserify'),
+      tls: false
     };
     return config;
   },
